48f090ae813c0e9d38a9b571db7a51577a5a6b90
Paritosh7 <paritosh7.sahni@gmail.com>
2021-11-28 20:30:16 +0530

    createStore basic implementation

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-/**A function to create brand new store */
+/** Library code
+ * A function to create brand new store */
 function createStore() {
   /** The store should have 4 parts
    * 1. The state
    * 2. Get the state
-   * 3. Listen to changes on the state
+   * 3. Listen to changes on the state (subscribing and unsubscribing to changes)
    * 4. Update the state
    */
 
+  // state
   let state = {};
   const listeners = [];
-
+  // getting the state
   const getState = () => state;
+
+  // listening to state
   const subscribe = (listener) => {
     listeners.push(listener);
+    // Returning a function to unsubscribe
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
   };
 
   return {
     getState,
     subscribe,
+    dispatch,
   };
 }
 
+/** App Code
+ * Reducer is a pure function
+ */
+function reducer(state = [], action) {
+  if (action.type === "ADD_TODO") {
+    state = state.concat([action.todo]);
+  }
+
+  return state;
+}
+
 const store = createStore();
-store.subscribe(() => console.log("The state has been updated"));
+const unsubscribe = store.subscribe(() =>
+  console.log("The state has been updated")
+);
+unsubscribe();
